19fe1e692702cdee46fe63afd34fbfba8346c5a5
Vlad Bardalez <bardalezvlad@gmail.com>
2016-03-05 13:24:45 -0500

    Cleaned up Routes

---
+++ routes/login.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var querystring = require('querystring');
+var router = express.Router();
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */ 
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+/* GET Spotify Login */
+router.get('/', function(req, res) {
+
+  var state = generateRandomString(16); 
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+
+module.exports = router;

f53a2136c0e23d7dd68b93c5c9c484faedada6ad
Vlad Bardalez <bardalezvlad@gmail.com>
2016-03-05 17:14:51 -0500

    Changed Redirect URI

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var SpotifyWebApi = require('spotify-web-api-node');
 
-var routes = require('./routes/index');
+var routes = require('./routes/index'); 
 var users = require('./routes/users');
 var auth = require('./routes/auth');
 

c4557c8d1e6b50f3b89a9f955c9214425e8b1354
Joshua Sayavong <jsayavon@uwaterloo.ca>
2016-03-05 08:53:23 -0500

    Added zurb foundation for apps

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var angularApp = angular.module('angularApp', [// Angular libraries
+// Foundation UI components
+'foundation']);
+
+angularApp.controller('homeController', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+});

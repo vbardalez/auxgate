a4a439fef86dbfec5d85e22be3f40e74a548c189
Joshua Sayavong <jsayavon@uwaterloo.ca>
2016-03-06 10:00:18 -0500

    fixed bad merge

--- js/app.js
+++ js/app.js
@@ -133,13 +133,7 @@ app.controller('eventController', ['$scope', '$http', 'ModalFactory', 'eventServ
                             trackId: _song.id
                         }
                         $http.post(url, data).then(() => {
-<<<<<<< HEAD
-                            
                                 $scope.playlist.songs.push(_song);
-=======
-                            $scope.playlist.songs.push(_song);
-                            //$scope.playlist.songs[0] hasSongs = true;
->>>>>>> 5b067fce19382d0849970e9a958d690228d8ea93
                         }, () => {
                             alert("update failed");
                         });/*
@@ -164,7 +158,6 @@ app.controller('eventController', ['$scope', '$http', 'ModalFactory', 'eventServ
                 }
             });
             modal.activate();
-
         }
 
         $scope.title = "Friday Nights at Stages";
